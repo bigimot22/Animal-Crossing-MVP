87c72179ed06951d45e5aeb2a56d34dc0e90fe3a
Johandre Delgado <bigimot22@gmail.com>
2023-03-04 06:30:15 -0800

    Add Project

---
+++ Animal Crossing MVP/Animal Crossing MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animal Crossing MVP
+//
+//  Created by Johandre Delgado Moreno on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+
+
+}
+
